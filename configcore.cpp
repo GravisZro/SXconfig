3cc9194c52823d36a190f40282fa2cb99cff62ce
GravisZro <github@adaptivetime.com>
2019-03-22 03:21:25 -0400

    use mountpoint.h and ditch anonymous sockets

--- configcore.cpp
+++ configcore.cpp
@@ -2,59 +2,57 @@
 
 // PDTK
 #include <cxxutils/syslogstream.h>
-
-#ifndef SCFS_PATH
-#define SCFS_PATH               "/svc"
-#endif
+#include <specialized/mountpoints.h>
 
 #ifndef CONFIG_USERNAME
 #define CONFIG_USERNAME         "config"
 #endif
 
-#ifdef ANONYMOUS_SOCKET
-#undef ANONYMOUS_SOCKET
-#endif
-#define ANONYMOUS_SOCKET        "\0"
-
 #ifndef CONFIG_IO_SOCKET
-#define CONFIG_IO_SOCKET        SCFS_PATH "/" CONFIG_USERNAME "/io"
+#define CONFIG_IO_SOCKET        "io"
 #endif
 
 #ifndef CONFIG_DIRECTOR_SOCKET
-#define CONFIG_DIRECTOR_SOCKET  SCFS_PATH "/" CONFIG_USERNAME "/director"
+#define CONFIG_DIRECTOR_SOCKET  "director"
 #endif
 
 ConfigCore::ConfigCore(void)
 {
-  if(m_config_server.bind(CONFIG_IO_SOCKET))
-    posix::syslog << posix::priority::info
-                  << "Config provider bound to socket file %1"
-                  << CONFIG_IO_SOCKET
-                  << posix::eom;
-  else if(m_config_server.bind(ANONYMOUS_SOCKET CONFIG_IO_SOCKET))
-    posix::syslog << posix::priority::info
-                  << "Config provider bound to anonymous socket %1"
-                  << CONFIG_IO_SOCKET
-                  << posix::eom;
-  else
+  if(scfs_path == nullptr)
+    reinitialize_paths();
+  if(scfs_path == nullptr)
+  {
     posix::syslog << posix::priority::error
-                  << "Unable to bind Config provider to %1"
-                  << CONFIG_IO_SOCKET
+                  << "SCFS is not mounted! Unable to bind Config and Config Director sockets."
                   << posix::eom;
+  }
+  else
+  {
+    std::string base = scfs_path;
+    base.push_back('/');
+    base.append(CONFIG_USERNAME);
+    base.push_back('/');
 
-  if(m_director_server.bind(CONFIG_DIRECTOR_SOCKET))
-    posix::syslog << posix::priority::info
-                  << "Config provider bound to %1"
-                  << CONFIG_DIRECTOR_SOCKET
-                  << posix::eom;
-  else if(m_director_server.bind(ANONYMOUS_SOCKET CONFIG_DIRECTOR_SOCKET))
-    posix::syslog << posix::priority::info
-                  << "Config provider bound to anonymous socket %1"
-                  << CONFIG_DIRECTOR_SOCKET
-                  << posix::eom;
-  else
-    posix::syslog << posix::priority::error
-                  << "Unable to bind Config provider to %1"
-                  << CONFIG_DIRECTOR_SOCKET
-                  << posix::eom;
+    if(m_config_server.bind((base + CONFIG_IO_SOCKET).c_str()))
+      posix::syslog << posix::priority::info
+                    << "Config provider bound to socket file %1"
+                    << base + CONFIG_IO_SOCKET
+                    << posix::eom;
+    else
+      posix::syslog << posix::priority::error
+                    << "Unable to bind Config provider to %1"
+                    << base + CONFIG_IO_SOCKET
+                    << posix::eom;
+
+    if(m_director_server.bind((base + CONFIG_DIRECTOR_SOCKET).c_str()))
+      posix::syslog << posix::priority::info
+                    << "Config Director provider bound to %1"
+                    << base + CONFIG_DIRECTOR_SOCKET
+                    << posix::eom;
+    else
+      posix::syslog << posix::priority::error
+                    << "Unable to bind Config Director provider to %1"
+                    << base + CONFIG_DIRECTOR_SOCKET
+                    << posix::eom;
+  }
 }
